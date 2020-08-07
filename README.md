e67f532b538c5ac239f017eba0e6a425a796f615
Bryan Schofield <bryan.schofield@libertyits.com>
2020-08-07 12:45:09 -0400

    cpu and memory wasting is now available

--- README.md
+++ README.md
@@ -9,15 +9,27 @@ It provides a simple rest application with following endpoints
 
 - `/hello` or `/hello/{status}`  
   Say hello and return some useful information. 
-  The HTTP status code can be provided. If omitted, 200 will be used.
+  The HTTP status code can be provided. 
+  If omitted, `200` will be used when healthy and `419` will be used when poisoned.
 
 - `/goodbye` or `/goodbye/{exitCode}`  
   Force the application to abruptly exit, useful for testing crash behavior.
   If `exitCode` is provided, it will be application exit code, 
   otherwise 0 will be used.
 
-- `/poison` and `/heal`
+- `/poison` and `/heal`  
   Poison the application, forcing the health checks to report UP or DOWN.
+  
+- `/busy` or `/busy/{seconds}`  
+   Consume a thread for a period of seconds before responding.
+   If `seconds` is not specified, the request will take 10 seconds to respond.
+
+- `/memory/consume` or `/memory/consume/{megabytes}` and `/memory/free`  
+   Consume a chunk of memory or free any consumed chunks.
+   If `megabytes` is specified, the application will consume approximately
+   the specified value in heap space.
+   If `megabytes` is not specified, approximately 1MB will be consumed.
+   Freeing release all consumed memory.
 
 ## Usage
 
@@ -49,12 +61,28 @@ Force application to be healthy.
 curl -XPOST http://exemplar.com/heal
 ```
 
+Consume 100MB of memory.
+```
+curl -XPOST http://exemplar.com/memory/consume/100
+```
+
+Free consumed memory.
+```
+curl -XPOST http://exemplar.com/memory/free
+```
+
+Consume 5 threads for 30 seconds.
+```
+for i in {1..5}; do curl -XPOST http://exemplar.com/busy/30 & done
+```
+
 ### Greeting response
 ```
 {
   "hostname": "Bryans-MacBook-Pro.local", // The application hostname
   "instance": 488928549,                  // A unique application instance id
-  "poisoned": false                       // Indicates whether the app is poisoned
+  "poisoned": false,                      // Indicates whether the app is poisoned
+  "wastedSpace": 0,                       // Approximate MBs of memory consumed
   "time": "2020-08-03T18:56:21.170030Z",  // The request time
   "requestCount": 5,                      // Cumulative request count for the instance
   "status": 200,                          // The supplied status code or 200
--- src/main/java/gov/va/api/health/exemplar/Controller.java
+++ src/main/java/gov/va/api/health/exemplar/Controller.java
@@ -1,14 +1,22 @@
 package gov.va.api.health.exemplar;
 
 import java.net.Inet4Address;
+import java.security.SecureRandom;
+import java.time.Duration;
 import java.time.Instant;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.atomic.AtomicInteger;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import lombok.Builder;
 import lombok.SneakyThrows;
 import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.MultiValueMap;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,10 +27,40 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(produces = {"application/json"})
 @Slf4j
+@Validated
 public class Controller {
 
   private static final int INSTANCE_ID = new Object().hashCode();
   private static final AtomicInteger REQUEST_COUNT = new AtomicInteger(0);
+
+  private static final List<double[]> WASTED_SPACE = new CopyOnWriteArrayList<>();
+
+  @PostMapping({"/busy", "/busy/{seconds}"})
+  long busy(@PathVariable(name = "seconds", required = false) @Min(1) @Max(300) Integer seconds) {
+    if (seconds == null) {
+      seconds = 10;
+    }
+    log.info("Randomly adding numbers for the next {} seconds", seconds);
+    var quitingTime = Instant.now().plusSeconds(seconds);
+    var lastReport = Instant.ofEpochMilli(0);
+    Random random = new SecureRandom();
+    int operations = 0;
+    long result = 0;
+    while (Instant.now().isBefore(quitingTime)) {
+      operations++;
+      if (random.nextBoolean()) {
+        result += random.nextInt();
+      } else {
+        result -= random.nextInt();
+      }
+      if (Duration.between(lastReport, Instant.now()).toSeconds() > 2) {
+        log.info("After {} operations, the value is {}", operations, result);
+        lastReport = Instant.now();
+      }
+    }
+    log.info("Wow. After {} operations, the result is {}", operations, result);
+    return result;
+  }
 
   @PostMapping({"goodbye", "/goodbye/{exit}"})
   void goodbye(@PathVariable(name = "exit", required = false) Integer exit) {
@@ -41,19 +79,45 @@ public class Controller {
       @RequestHeader MultiValueMap<String, String> headers,
       @PathVariable(required = false, name = "status") Integer status) {
     if (status == null) {
-      status = 200;
+      status = PoisonHealthCheck.POISONED.get() ? 418 : 200;
     }
     var greeting =
         Greeting.builder()
             .instance(INSTANCE_ID)
             .requestCount(REQUEST_COUNT.incrementAndGet())
             .poisoned(PoisonHealthCheck.POISONED.get())
+            .wastedSpace(WASTED_SPACE.size())
             .time(Instant.now())
             .hostname(Inet4Address.getLocalHost().getHostName())
             .headers(headers)
             .status(status)
             .build();
     return ResponseEntity.status(status).body(greeting);
+  }
+
+  @PostMapping({"/memory/consume", "/memory/consume/{chunks}"})
+  int memoryConsume(@PathVariable(value = "chunks", required = false) Integer chunks) {
+    if (chunks == null) {
+      chunks = 1;
+    }
+    for (int i = 0; i < chunks; i++) {
+      memoryConsumeOneChunk();
+    }
+    return WASTED_SPACE.size();
+  }
+
+  private void memoryConsumeOneChunk() {
+    Random random = new SecureRandom();
+    final int doublesPerMB = (1024 * 1024) / (8 + 8);
+    WASTED_SPACE.add(random.doubles(doublesPerMB).toArray());
+  }
+
+  @PostMapping({"/memory/free"})
+  int memoryFree() {
+    int items = WASTED_SPACE.size();
+    WASTED_SPACE.clear();
+    Runtime.getRuntime().gc();
+    return items;
   }
 
   @PostMapping({"/poison"})
@@ -71,5 +135,6 @@ public class Controller {
     MultiValueMap<String, String> headers;
     int status;
     boolean poisoned;
+    int wastedSpace;
   }
 }
